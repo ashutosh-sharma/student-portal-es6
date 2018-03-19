e8ab7225fc3d6009ed33d1d63fac78f5c9cd8df3
Ashutosh Sharma <mainhuashutosh@gmail.com>
2018-03-19 16:06:00 +0530

    adding delete functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
 
-function editStudentData() {
+/* To edit a stored record.*/
+function editStudentData(but) {
+    
+    let id = but.getAttribute('data-id')
+
+    //window.location="editItem.html";
+    editPage(id);
+
+    // let name = document.getElementById("sname");
+    // name.innerHTML = newName;
+}
+
+function editPage(id){
+//    let id = but.getAttribute('data-id');
+    window.location = 'editItem.html';
+    
+    alert(id);
+    let name = document.getElementById('sname');
+    name.setAttribute(value) = id;
 
 }
 
-// TODO : get values from form and update here
+
+/* To delete a stored record. */
+function deleteStudentData(but){
+    let id = but.getAttribute('data-id');
+    
+    sessionStorage.removeItem(id);
+    window.location.reload(true);
+}
+
+
 function addStudentData() {
     let name = document.getElementById('sname').value;
     let id = document.getElementById('sid').value;
@@ -47,10 +74,16 @@ function showData() {
         let id = row.insertCell(1);
         let year = row.insertCell(2);
         let stream = row.insertCell(3);
+        let options = row.insertCell(4);
 
+        /* Using dataId attribute of HTML5 to store the Id of the record, this will make edit and delete operations easy */
+        let eButton = "<button class='btn btn-success' onclick='editStudentData(this)' data-id=" + sessionStorage.key(i) + ">Edit</button>";
+        let dButton = "<button class='btn btn-danger' onclick='deleteStudentData(this)' data-id=" + sessionStorage.key(i) + ">Delete</button>";
+        
         name.innerHTML = obj.name;
         id.innerHTML = sessionStorage.key(i);
         year.innerHTML = obj.year;
         stream.innerHTML = obj.stream;
+        options.innerHTML = eButton + ' ' + dButton;
     }
 }
