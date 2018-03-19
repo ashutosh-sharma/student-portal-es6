cc8eefc78ba0d72e5db7583a690b30f983b4ec09
Ashutosh Sharma <mainhuashutosh@gmail.com>
2018-03-19 13:02:26 +0530

    functionality added - adding and fetching records from storage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-class Student {
-    constructor(name, year, stream) {
-        this.name = name;
-        this.year = year;
-        this.stream = stream;
-    }
-}
 
 function editStudentData() {
 
@@ -12,25 +5,33 @@ function editStudentData() {
 
 // TODO : get values from form and update here
 function addStudentData() {
-    //    sessionStorage.setItem("1", "value");
-    let formValues = document.getElementById("myForm");
+    let name = document.getElementById('sname').value;
+    let id = document.getElementById('sid').value;
+    let year = document.getElementById('syear').value;
+    let stream = document.getElementById('sstream').value;
+    
+    /* Form validation -- the fields should not be empty. */
+    if (name==null || name=="", id == null || id =="", year == null || year=="",stream==null || stream=="")
+    {
+            alert("Please fill all required fields.");
+            return false;
+    }
 
-    let name = formValues.elements[0].value;
-    let id = document.getElementById(sid).value;
-    let year = docuement.getElementById(syear).value;
-    let stream = document.getElementById(sstream).value;
+    /* Making a JSON object using the form values */
+    let studentObj = {'name' : name , 'year' : year, 'stream' : stream};
 
-    let studentObj = {'name' : name /*, 'year' : year, 'stream' : stream*/};
-
+    /* Pushing record to sessionStorage after stringify JSON Object*/
     sessionStorage.setItem(id, JSON.stringify(studentObj));
-
+    
+    /* An alert message */
+    alert("Student record added");
 }
 
+
+/*
+* A funtion to show all the records present in the session Storage.
+*/
 function showData() {
-    
-    //let stu = sessionStorage.getItem(151);
-    //let obj =  JSON.parse(retrievedObject);
-    //document.getElementById('result').innerHTML = obj.name;
 
     for (let i = 0; i < sessionStorage.length; i++) {
 
@@ -39,8 +40,8 @@ function showData() {
 
         let table = document.getElementById("dataTable");
 
-        // table.length in place of 0
-        let row = table.insertRow(0);
+        /* -1 argument will append in the table*/
+        let row = table.insertRow(-1);
 
         let name = row.insertCell(0);
         let id = row.insertCell(1);
@@ -49,9 +50,7 @@ function showData() {
 
         name.innerHTML = obj.name;
         id.innerHTML = sessionStorage.key(i);
-        //year.innserHTML = obj.year;
-        //stream.innerHTML = obj.stream;
+        year.innerHTML = obj.year;
+        stream.innerHTML = obj.stream;
     }
-
-    //    document.getElementById('result').innerHTML = sessionStorage.getItem(1);
 }
