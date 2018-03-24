5524add395f78165000908e81705b591d72cb557
Ashutosh Sharma <mainhuashutosh@gmail.com>
2018-03-24 10:53:05 +0530

    Adding proper comments to make it easy to understand

--- js/main.js
+++ js/main.js
@@ -2,48 +2,19 @@
     This javascript is based upon es6 principles.
 */
 
-/* Putting id in the sessionStorage so that id can be to fill data on editItem.html */
-const editStudentData = (editButton) => {
-    let id = editButton.getAttribute('data-id')
-    // Using local storage to store id of record which we want to edit */
-    sessionStorage.setItem("editId", id);
-    window.location.href = "editItem.html";
-}
-
-/* To fill the data in the form fields when we want to edit an item | Called onload() of editItem page's body*/
-const fillEditPage = () => {
-    let id = sessionStorage.getItem('editId');
-
-    let stu = sessionStorage.getItem(id);
-    let obj = JSON.parse(stu);
-
-    document.getElementById("sname").value = obj.name;
-    document.getElementById("sid").value = id;
-    document.getElementById("syear").value = obj.year;
-    document.getElementById("sstream").value = obj.stream;
-
-    /* Have to remove record here beacuse the utility module in addStudentData() in not working */
-    // sessionStorage.removeItem(id);
-    // sessionStorage.removeItem('editId');
-}
-
-//const toggleAddDetails = () =>{
-
-/* To add/edit a student record to sessionStorage */
-//function addStudentData() {
-const addStudentData = () => {
+/*
+*   This function will be called when addStudent or editStudent forms will be submitted.
+*/
+const onFormSub = () => {
     let name = document.getElementById('sname').value;
     let id = document.getElementById('sid').value;
     let year = document.getElementById('syear').value;
     let stream = document.getElementById('sstream').value;
 
-    
-    // A utility module for editItem page : I DONT KNOW WHY IT'S NOT WORKING
+    // A utility module for editItem page- removing the record when form is submitted
     if(sessionStorage.getItem('editId')){
         let remId = sessionStorage.getItem('editId');
-        console.log(remId);
         sessionStorage.removeItem(remId);
-        console.log('here!!!!!!!!!!!!!!');
         // removing the Id of the item from sessionStorage which we want to edit
         sessionStorage.removeItem('editId');
     }
@@ -54,7 +25,7 @@ const addStudentData = () => {
         return false;
     }
 
-    /* Ensuring unique keys should be allowed. */
+    // Ensuring unique keys should be allowed.
     for (let i = 0; i < sessionStorage.length; i++) {
         if (sessionStorage.key(i) == id) {
             alert("Id already exist, cannot insert more than one record using one ID.");
@@ -70,27 +41,67 @@ const addStudentData = () => {
 
     // Redirecting back to home page
     alert("Done. Redirect to home page!");
-
     window.location.href = "index.html";
+
     return false;
+}
+
+/* This will be called when user clicks on edit button of a record
+*   Putting id in the sessionStorage so that id can be to fill data on editItem.html 
+*/
+const editStudentData = (editButton) => {
+    // Using 'data' attribute of HTML5 to get the id of the record which we want to edit 
+    let id = editButton.getAttribute('data-id')
+
+    // Using session storage to store id of record,
+    // We will use this id to fill the records data in form fields on editItem.html
+    sessionStorage.setItem("editId", id);
+    
+    // Rendering editIten.html form webPage
+    window.location.href = "editItem.html";
+}
+
+/* This will be called onload() of editItem.html ,to fill the data in the form fields. */
+const fillEditFormPage = () => {
+    // fetching 'editId' from the sessionStorage
+    let id = sessionStorage.getItem('editId');
+
+    let stu = sessionStorage.getItem(id);
+    let obj = JSON.parse(stu);
+
+    // filling the data in form
+    document.getElementById("sname").value = obj.name;
+    document.getElementById("sid").value = id;
+    document.getElementById("syear").value = obj.year;
+    document.getElementById("sstream").value = obj.stream;
 }
 
 /* To delete a stored record. */
 const deleteStudentData = (but) => {
-
+    // Using 'data' attribute of HTML5 to get the id of the record we want to delete
     let id = but.getAttribute('data-id');
+
+    // A confirm box to alert the user
     if (confirm('Are You Sure?')) {
         sessionStorage.removeItem(id);
     }
+
+    // Reloading the webPage
     window.location.reload(true);
 }
 
 
-// A funtion to show all the records present in the session Storage.
+/* A funtion to show all the records present in the session Storage.
+*   Called onload() of index.html- will all the record in session storage
+*/
 const showData = () =>{
 
+    // Looping through the sessionStorage to show data in table
     for (let i = 0; i < sessionStorage.length; i++) {
+        
+        // To continue the loop when 'editId' is there coz thats not a record
         if(sessionStorage.key(i) == 'editId')   continue;
+
         let stu = sessionStorage.getItem(sessionStorage.key(i));
         let obj = JSON.parse(stu);
 
@@ -105,7 +116,7 @@ const showData = () =>{
         let stream = row.insertCell(3);
         let options = row.insertCell(4);
 
-        /* Using dataId attribute of HTML5 to store the Id of the record, this will make edit and delete operations easy */
+        /* Using 'data' attribute of HTML5 to store the Id of the record, this will make edit and delete operations easy */
         let eButton = `<button class='btn btn-success' onclick='editStudentData(this)' data-id=${sessionStorage.key(i)}><i class='fa fa-pencil' aria-hidden='true'></i> Edit</button>`;
         let dButton = `<button class='btn btn-danger' onclick='deleteStudentData(this)' data-id=${sessionStorage.key(i)}><i class='fa fa-trash' aria-hidden='true'></i> Delete</button>`;
 
@@ -119,10 +130,14 @@ const showData = () =>{
     }
 }
 
-/* A function to delete all data, but adding the dummy data AS PER REQUIREMENTS. */
+/* A function to delete all records. */
 const deleteAllData = () => {
+    // Clearing the sessionStorage
     sessionStorage.clear();
 
+    // A confirm box to alert the user
     confirm('Wanna delete all the data?')
     window.location.reload(true)
 }
+
+/* The End.*/
